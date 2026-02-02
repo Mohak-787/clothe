ced2e45f30bf46a92dc134e7d829de8e5ae7d373
Mohak-787 <mohakdevkota62@gmail.com>
2026-02-02 07:25:09 +0545

    feat: implement product CRUD operations in admin controller

--- backend/src/controllers/admin.controller.js
+++ backend/src/controllers/admin.controller.js
@@ -1,3 +1,43 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Product } from "../models/product.model.js"
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
+
+const createProduct = asyncHandler(async (req, res) => {
+
+});
+
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await Product.find({});
+
+    if (!products) {
+        return res.status(204).json(
+            new ApiError(204, "No products available")
+        )
+    }
+
+    res.status(200).json(
+        new ApiResponse(200, products, "Products fetched successfully")
+    );
+});
+
+const updateProduct = asyncHandler(async (req, res) => {
+
+});
+
+const deleteProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json(
+            new ApiError(404, "Product not found")
+        );
+    }
+
+    await Product.findByIdAndDelete(id)
+    res.status(204).json(
+        new ApiResponse(204, "", "Product deleted successfully")
+    );
+});
+
+export { getProducts, updateProduct, deleteProduct }
