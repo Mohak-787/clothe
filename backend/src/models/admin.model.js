b471d69f1de899abc2049a50fcacf36fe2ad32d4
Mohak-787 <mohakdevkota62@gmail.com>
2026-02-04 09:32:57 +0545

    fix: refresh token in admin model

--- backend/src/models/admin.model.js
+++ backend/src/models/admin.model.js
@@ -27,7 +27,7 @@ const userSchema = new Schema({
         type: String,
         required: [true, "Password is required"]
     },
-    refershToken: {
+    refreshToken: {
         type: String
     }
 }, { timestamps: true });
